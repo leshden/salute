9cc19f20f381b0228f7b299a9894be099771f192
Viktor <leshden@yandex.ru>
2018-04-15 20:49:48 +0300

    add gradient

--- salute/salute/src/shaders/particle.fs
+++ salute/salute/src/shaders/particle.fs
@@ -10,7 +10,10 @@ void main()
 	vec4 texColor = texture(sprite, TexCoords);
 	if(texColor.a < 0.1)
 		discard;
-	color = texColor * ParticleColor;
+	vec2 resolution = vec2(800, 600);
+	vec2 position = gl_FragCoord.xy / resolution.xy;
+	vec4 newColor = vec4(ParticleColor.r, position.y, ParticleColor.b, ParticleColor.a);
+	color = texColor * newColor;
 	
 	//vec4 texColor = (texture(sprite, TexCoords) * ParticleColor);
    // if(texColor.x == 1.0 && texColor.y == 1.0 && texColor.z == 1.0)
